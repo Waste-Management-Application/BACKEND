8426c2135dcd6158e5b0dad2d138123fc7ff36bd
Barden Daniel <danielquavohunchjnr@gmail.com>
2023-08-02 00:32:05 +0000

    Updates

--- connect.js
+++ connect.js
@@ -18,7 +18,8 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex:true
+  useCreateIndex:true,
+
   
 })
   .then(() => {
